2e151ddbc5d88491c4dddd17f9f0e183a35220b9
Ananthu <ananthulb@outlook.com>
2022-01-14 17:01:06 +1300

    service section done

--- react-app/src/pages/index.tsx
+++ react-app/src/pages/index.tsx
@@ -4,6 +4,7 @@ import Sidebar from '../components/Sidebar';
 import HeroSection from '../components/HeroSection';
 import InfoSection from '../components/InfoSection';
 import { homeObjOne, homeObjTwo, homeObjThree } from '../components/InfoSection/Data';
+import Services from '../components/Services';
 function Home() {
     const [isOpen, setIsOpen] = useState(false);
     const toggle = () => {
@@ -18,6 +19,7 @@ function Home() {
             <HeroSection />
             <InfoSection {...homeObjOne} />
             <InfoSection {...homeObjTwo} />
+            <Services />
             <InfoSection {...homeObjThree} />
         </>
     )
